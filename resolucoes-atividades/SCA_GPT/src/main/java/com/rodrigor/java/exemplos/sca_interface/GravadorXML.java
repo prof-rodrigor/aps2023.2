852fe39900f6500dcfe15672ce654287b5c3d56b
Rodrigo Rebouças <rodrigor@dcx.ufpb.br>
2024-03-11 10:01:05 -0300

    20230311

--- resolucoes-atividades/SCA_GPT/src/main/java/com/rodrigor/java/exemplos/sca_interface/GravadorXML.java
+++ resolucoes-atividades/SCA_GPT/src/main/java/com/rodrigor/java/exemplos/sca_interface/GravadorXML.java
@@ -1,80 +1,78 @@
 package com.rodrigor.java.exemplos.sca_interface;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 public class GravadorXML implements GravadorAluno {
 
-    private static final String XML_FILE = "alunos.xml";
+    private final String arquivoXML;
+
+    public GravadorXML(String arquivoXML) {
+        this.arquivoXML = arquivoXML;
+    }
 
     @Override
     public void salvarAluno(Aluno aluno) {
-        try {
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document;
-            Element root;
-
-            File xmlFile = new File(XML_FILE);
-            if (xmlFile.exists()) {
-                document = documentBuilder.parse(xmlFile);
-                root = document.getDocumentElement();
-            } else {
-                document = documentBuilder.newDocument();
-                root = document.createElement("alunos");
-                document.appendChild(root);
-            }
-
-            Element alunoElement = document.createElement("aluno");
-            alunoElement.setAttribute("matricula", aluno.getMatricula());
-            alunoElement.appendChild(document.createElement("nome")).appendChild(document.createTextNode(aluno.getNome()));
-
-            root.appendChild(alunoElement);
-
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(xmlFile);
-            transformer.transform(source, result);
-        } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
-            e.printStackTrace();
-        }
+        List<Aluno> alunos = recuperarAlunos();
+        alunos.add(aluno);
+        salvarListaAlunos(alunos);
     }
 
     @Override
     public List<Aluno> recuperarAlunos() {
-        List<Aluno> alunos = new ArrayList<>();
         try {
-            File xmlFile = new File(XML_FILE);
-            if (!xmlFile.exists()) {
-                return alunos;
+            if (!Files.exists(Paths.get(arquivoXML))) {
+                return new ArrayList<>();
             }
-
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(xmlFile);
-            document.getDocumentElement().normalize();
-
-            NodeList nodeList = document.getElementsByTagName("aluno");
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    String nome = element.getElementsByTagName("nome").item(0).getTextContent();
-                    String matricula = element.getAttribute("matricula");
-                    alunos.add(new Aluno(nome, matricula));
-                }
-            }
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+            JAXBContext context = JAXBContext.newInstance(Alunos.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Alunos alunos = (Alunos) unmarshaller.unmarshal(new File(arquivoXML));
+            return alunos.getLista();
+        } catch (JAXBException e) {
             e.printStackTrace();
         }
-        return alunos;
+        return new ArrayList<>();
+    }
+
+    private void salvarListaAlunos(List<Aluno> alunos) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Alunos.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(new Alunos(alunos), new FileWriter(arquivoXML));
+        } catch (JAXBException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Alunos {
+        private List<Aluno> lista;
+
+        public Alunos() {
+            this.lista = new ArrayList<>();
+        }
+
+        public Alunos(List<Aluno> lista) {
+            this.lista = lista;
+        }
+
+        public List<Aluno> getLista() {
+            return lista;
+        }
+
+        public void setLista(List<Aluno> lista) {
+            this.lista = lista;
+        }
     }
 }
